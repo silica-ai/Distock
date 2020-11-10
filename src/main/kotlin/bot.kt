cfda9fc340f71f1affdef2d4c61c6be8987e934c
Phaniraj Aenugula <paenugul@calpoly.edu>
2020-11-09 23:12:03 -0800

    initial bot connected

--- src/main/kotlin/bot.kt
+++ src/main/kotlin/bot.kt
@@ -1,3 +1,16 @@
-fun main() {
-    println("helloo")
+import com.jessecorbett.diskord.dsl.bot
+import com.jessecorbett.diskord.dsl.command
+import com.jessecorbett.diskord.dsl.commands
+
+const val BOT_TOKEN = "NzcxOTUzMjQzMzAzNTc1NTUz.X5znew.PWeM8zE5qaIU0Ou5mMD7hU0xiAo"
+
+suspend fun main() {
+    bot(BOT_TOKEN) {
+        commands {
+            command("ping") {
+                reply("pong")
+                delete()
+            }
+        }
+    }
 }
