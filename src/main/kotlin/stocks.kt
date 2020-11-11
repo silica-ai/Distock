7badd163ca9ea307af927c9c5890979c9303d6d2
Phaniraj Aenugula <paenugul@calpoly.edu>
2020-11-10 18:57:30 -0800

    Test Fix

--- src/main/kotlin/stocks.kt
+++ src/main/kotlin/stocks.kt
@@ -7,5 +7,8 @@ fun getTickerPrice(ticker : String) : Message{
     // make a request to yahoo finance to get price
     val doc = Jsoup.connect("https://finance.yahoo.com/quote/$ticker?p=$ticker").timeout(10000).get()
     val tickerPrice = doc.getElementsByClass("Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)").text()
+    if(ticker.isBlank()) {
+        return Message("The ticker: $ticker does not exist.")
+    }
     return Message("Ticker price of $ticker is $tickerPrice")
 }
