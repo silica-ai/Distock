69861426219aa8e69691f0e618fe86fe2cac7c69
Phaniraj Aenugula <paenugul@calpoly.edu>
2020-11-10 20:21:46 -0800

    Added Yahoo Finance API

--- src/main/kotlin/stocks.kt
+++ src/main/kotlin/stocks.kt
@@ -1,14 +1,14 @@
 import org.jsoup.Jsoup
+import yahoofinance.Stock
+import yahoofinance.YahooFinance
 
-data class Message
-    (val testString : String)
+data class Message(val testString : String)
 
 fun getTickerPrice(ticker : String) : Message{
     // make a request to yahoo finance to get price
-    val doc = Jsoup.connect("https://finance.yahoo.com/quote/$ticker?p=$ticker").timeout(10000).get()
-    val tickerPrice = doc.getElementsByClass("Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(ib)").text()
-    if(ticker.isBlank()) {
-        return Message("The ticker: $ticker does not exist.")
-    }
-    return Message("Ticker price of $ticker is $tickerPrice")
+    val tickerStock = YahooFinance.get(ticker) ?: return Message("Stock $ticker not available")
+    val price = tickerStock.getQuote().price
+
+    return Message("Ticker price of $ticker is $price")
+
 }
